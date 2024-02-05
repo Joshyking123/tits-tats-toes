f641ab4c6163808aff717d1a5e5df33b9d26a83f
Joshyking123 <144693159+Joshyking123@users.noreply.github.com>
2024-02-05 14:00:39 +0100

    banana

--- script.js
+++ script.js
@@ -1,28 +1,4 @@
-<main class="background">
-    <section class="title">
-        <h1>Tic Tac Toe</h1>
-    </section>
-    <section class="display">
-        Player<span class="display-player playerX">X</span>'s turn
-    </section>
-    <section class="container">
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
-    </section>
-    <section class="display announcer hide"></section>
-    <section class="controls">
-        <button id="reset">Reset</button>
-    </section>
-</main>
 
-<script>
 const tiles = Array.from(document.querySelectorAll(".tile"));
 const playerDisplay = document.querySelector(".display-player");
 const resetButton = document.querySelector("#reset");
@@ -61,4 +37,3 @@ tiles.forEach( (tile, index) => {
 })
 
 resetbutton.addEventListener("click", resetboard)
-</script>
